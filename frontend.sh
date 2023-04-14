93153f803b2d59e561c27b7bf19ade0ed33c3633
91996 <mukunca50@gmail.com>
2023-04-15 01:44:12 +0530

    common.sh

--- frontend.sh
+++ frontend.sh
@@ -1,14 +1,34 @@
-echo -e "\e[36minstall nginx\e[0m"
-yum install nginx -y
-echo -e "\e[36mremoving old content\e[0m"
-rm -rf /usr/share/nginx/html/*
-echo -e "\e[36mdownloading frontend content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-echo -e "\e[36mExtrating frontend content\e[0m"
+source common.sh
+
+print_head "Installing nginx"
+yum install nginx -y &>>${log_file}
+status_check $?
+
+print_head "Removing Old Content"
+rm -rf /usr/share/nginx/html/* &>>${log_file}
+status_check $?
+
+print_head "Downloading Frontend Content"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
+status_check $?
+
+print_head "Extracting Downloaded Frontend"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-cp configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
-echo -e "\e[36menable nginx\e[0m"
-systemctl enable nginx
-echo -e "\e[36mrestart nginx\e[0m"
-systemctl restart nginx
+unzip /tmp/frontend.zip &>>${log_file}
+status_check $?
+
+print_head "Copying Nginx Config for RoboShop"
+cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
+status_check $?
+
+print_head "Enabling nginx"
+systemctl enable nginx &>>${log_file}
+status_check $?
+
+print_head "Starting nginx"
+systemctl restart nginx &>>${log_file}
+status_check $?
+
+
+## If any command is errored or failed, we need to stop the script
+# Status of a command need to be printed.
